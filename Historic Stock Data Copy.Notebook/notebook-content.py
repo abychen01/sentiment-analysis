09817402b03ca260f4922919f9095b28c9387201
abychen01 <87286482+abychen01@users.noreply.github.com>
2025-12-11 20:52:17 +0530

    Committing 1 item from workspace fa56728c-4e8f-4cf0-b06b-130982363f52

---
+++ Historic Stock Data Copy.Notebook/notebook-content.py
@@ -0,0 +1,108 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "8c338b2f-ab3a-4354-bdc7-d89db7abf459",
+# META       "default_lakehouse_name": "Stock",
+# META       "default_lakehouse_workspace_id": "fa56728c-4e8f-4cf0-b06b-130982363f52",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "8c338b2f-ab3a-4354-bdc7-d89db7abf459"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+pip install yfinance
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+import yfinance as yf
+
+from pyspark.sql.functions import col, lit, DataFrame, desc
+from pyspark.sql.types import StructType, StructField, ArrayType, StringType
+from functools import reduce
+# Define stock tickers
+ticker_list = ["TSLA", "MSFT", "AAPL", "NVDA", "GOOGL", "AMZN", "META", "AVGO", "TSM"]
+# Define schema
+schema = StructType([
+    StructField("tickers", ArrayType(StringType()), True)
+])
+# Create DataFrame of tickers
+ticker_df = spark.createDataFrame([(ticker_list,)], schema=schema)
+# Extract ticker list from DataFrame
+tickers = ticker_df.collect()[0]["tickers"]  # Convert PySpark DataFrame to Python list
+all_stock_data = []
+for ticker in tickers:
+    # Fetch historical data from Yahoo Finance
+    df = yf.Ticker(ticker).history(period="3y").reset_index()
+    # Convert Pandas DataFrame to PySpark DataFrame
+    spark_df = spark.createDataFrame(df)
+    # Select relevant columns and add ticker name
+    spark_df = spark_df.select("Date", "Close").withColumn("ticker", lit(ticker))
+    # Collect all stock data
+    all_stock_data.append(spark_df)
+# Combine all DataFrames into one
+final_spark_df = reduce(DataFrame.union, all_stock_data)
+final_spark_df = final_spark_df.withColumn("Date",col("Date").cast("date"))
+display(final_spark_df.sort(desc("Date")))
+
+final_spark_df.write.mode("overwrite").format("delta").saveAsTable("stock_data")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+final_spark_df.write.mode("overwrite").format("delta").saveAsTable("stock_data")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+df = spark.read.table("stock_data")
+print(df.count())
+display(df.sort(desc("Date")))
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
