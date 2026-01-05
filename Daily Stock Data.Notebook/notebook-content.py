9012cf656f2c796bf84b6ec2a2aadfbc7280f49a
abychen01 <87286482+abychen01@users.noreply.github.com>
2026-01-06 02:35:18 +0530

    Committing 2 items from workspace a2d18893-d874-4d0f-83d6-91d2ed3d9dfa

--- Daily Stock Data.Notebook/notebook-content.py
+++ Daily Stock Data.Notebook/notebook-content.py
@@ -33,6 +33,97 @@ pip install yfinance
 
 # CELL ********************
 
+#temp...
+import pyodbc, os
+from pyspark.sql.functions import col, lit
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+
+df_creds = spark.read.parquet('Files/creds')
+
+os.environ["AZURE_CLIENT_ID"] = df_creds.collect()[0]["AZURE_CLIENT_ID"]
+os.environ["AZURE_TENANT_ID"] = df_creds.collect()[0]["AZURE_TENANT_ID"]
+os.environ["AZURE_CLIENT_SECRET"] = df_creds.collect()[0]["AZURE_CLIENT_SECRET"]
+
+
+vault_url = "https://vaultforfabric.vault.azure.net/"
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=vault_url, credential=credential)
+
+password = client.get_secret("sql-server-password").value
+
+conn_str = (
+            f"DRIVER={{ODBC Driver 18 for SQL Server}};"
+            f"SERVER=tcp:myfreesqldbserver66.database.windows.net,1433;"
+            f"DATABASE=myFreeDB;"
+            f"UID=admin2;"
+            f"PWD={password};"
+            f"Encrypt=yes;"
+            f"TrustServerCertificate=yes;"
+            f"Connect Timeout=30;"
+        )
+
+with pyodbc.connect(conn_str, autocommit=True) as conn:
+    with conn.cursor() as cursor:
+        cursor.execute("""
+            SELECT max(Date) FROM stock_data 
+        """)
+
+          
+        while True:
+            result = cursor.fetchall()
+            if result:
+                print(result[0])
+            if not cursor.nextset():
+                break
+
+
+import yfinance as yf
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+from functools import reduce
+
+# Deleting the temp_stock_data table
+spark.sql("DROP TABLE IF EXISTS temp_stock_data")
+
+# Define stock tickers
+ticker_list = ["TSLA", "MSFT", "AAPL", "NVDA", "GOOGL", "AMZN", "META", "AVGO", "TSM"]
+# Define schema
+schema = StructType([
+    StructField("tickers", ArrayType(StringType()), True)
+])
+# Create DataFrame of tickers
+ticker_df = spark.createDataFrame([(ticker_list,)], schema=schema)
+# Extract ticker list from DataFrame
+tickers = ticker_df.collect()[0]["tickers"]  # Convert PySpark DataFrame to Python list
+all_stock_data = []
+
+
+for ticker in tickers:
+    # Fetch historical data from Yahoo Finance
+    df = yf.Ticker(ticker).history(start=latest_date).reset_index()
+    # Convert Pandas DataFrame to PySpark DataFrame
+    spark_df = spark.createDataFrame(df)
+    # Select relevant columns and add ticker name
+    spark_df = spark_df.select("Date", "Close").withColumn("ticker", lit(ticker))
+    # Collect all stock data
+    all_stock_data.append(spark_df)
+
+# Combine all DataFrames into one
+final_spark_df = reduce(DataFrame.union, all_stock_data)
+final_spark_df = final_spark_df.withColumn("Date",final_spark_df.Date.cast(DateType()))
+#final_spark_df.write.mode("overwrite").format("delta").saveAsTable("temp_stock_data")
+display(final_spark_df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
 import yfinance as yf
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
